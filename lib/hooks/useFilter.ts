e76ca89d030fdd2ac6a18590453ba9c535b89c1c
Maxim Semenov <semenovms124@gmail.com>
2022-08-22 20:30:38 +0700

    fix: fixed hook logic

--- lib/hooks/useFilter.ts
+++ lib/hooks/useFilter.ts
@@ -10,20 +10,19 @@ export function useFilter<T extends IFilterObject>(items: Array<T>, options: IFi
   const result = useRef<Array<T>>(items);
 
   useMemo(() => {
-    if (filter.value) {
-      result.current = items.filter((el: T) => (el[filter.name as keyof T] as unknown as string).split(' ')
-        .join('')
-        .toLowerCase()
-        .includes(filter.value.toLowerCase())
-      )
-    }
+    if (!filter.value) return result.current = items;
+    result.current = items.filter((el: T) => (el[filter.name as keyof T] as unknown as string).split(' ')
+      .join('')
+      .toLowerCase()
+      .includes(filter.value.toLowerCase())
+    )
   }, [items, filter.value, filter.name])
 
   useMemo(() => {
     sort === 'ASC' ?
       result.current = result.current.sort((a, b) => a.id - b.id) :
       result.current = result.current.sort((a, b) => b.id - a.id)
-  }, [result, sort])
+  }, [result.current, sort])
 
   return result.current
 }
